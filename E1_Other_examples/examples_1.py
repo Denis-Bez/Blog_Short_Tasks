2bb10c4d80b79d274b140c565e53c7a95cbbcc1d
Denis-Bez <v417459@yandex.ru>
2023-02-06 11:38:41 +0500

    Testing how abort cicle iteration

--- E1_Other_examples/examples_1.py
+++ E1_Other_examples/examples_1.py
@@ -5,5 +5,14 @@ def search_middle():
     print((s := word)[-(len(s) + 1) // 2])
 
 
+# How abort current cicle iteration
+def abort_cicle_iteration():
+    array = [1, 2, 3, 4, 5]
+    for i in array:
+        if i == 3:
+            continue
+        print(i)
+
+
 if __name__ == '__main__':
-    search_middle()
+    abort_cicle_iteration()
